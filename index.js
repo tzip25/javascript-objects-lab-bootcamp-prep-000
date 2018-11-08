8eb8ca28412a580e9a497b2e30f99bf3ed3aec23
Tali Scheer <tali.scheer@gmail.com>
2018-11-08 21:28:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,3 +18,8 @@ function deleteFromObjectByKey(obj, key) {
   delete newObj[key];
   return newObj;
 }
+
+function destructivelyDeleteFromObjectByKey(obj, key) {
+  delete obj[key];
+  return obj;
+}
