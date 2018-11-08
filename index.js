cc5d270bcaf6ebc7c4a3965fb8849f93c24c69d8
Tali Scheer <tali.scheer@gmail.com>
2018-11-08 21:23:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ var recipes = {
 function updateObjectWithKeyAndValue(obj, key, val) {
 return Object.assign({}, obj, { [key]: val } );
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, val) {
+  obj[key] = val;
+  return obj;
+}
